7e018323cea7cb3f1b8f313a6d4e2993cc86ffe3
Jonathan Choy <jonathan.j.choy+daybreak@gmail.com>
2013-12-23 00:12:48 -0500

    Create Test example.

---
+++ Appharbor-Logentres-Integration-2013/Appharbor-Logentres-Integration-2013/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Appharbor_Logentres_Integration_2013.MvcApplication" Language="C#" %>
---
+++ Appharbor-Logentres-Integration-2013/Appharbor-Logentres-Integration-2013/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using log4net;
+
+namespace Appharbor_Logentres_Integration_2013
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof (MvcApplication));
+        protected void Application_Start()
+        {
+            logger.Info("Started example application.");
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            logger.ErrorFormat("{0} sent error with details {1}", sender, e);
+        }
+    }
+}
